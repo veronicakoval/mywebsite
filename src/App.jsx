0f20a4f14b6b063c9fae7df10418009f9521e222
veronicakoval <vk2560@barnard.edu>
2025-01-18 00:25:50 -0500

    updated homepage

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,22 @@ import NYC from "./pages/NYC";
 import Ireland from "./pages/ireland";
 import AboutMe from "./pages/aboutme";
 import Navbar from "./components/navbar";
+import walking_dog from "./pictures/walking_dog.gif";
 import { Routes, Route, HashRouter } from "react-router-dom";
 import "./styles.css";
+
+// Add the Home component right in App.jsx
+function Home() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <img 
+        src={walking_dog}  
+        alt="Walking dog animation"
+        className="w-64 h-64"
+      />
+    </div>
+  );
+}
 
 function App() {
   return (
@@ -16,6 +30,8 @@ function App() {
       <Navbar></Navbar>
       <div>
         <Routes>
+          {/* Add the home route as the first route */}
+          <Route path="/" element={<Home />} />
           <Route path="/NYC" element={<NYC />} />
           <Route path="/Ireland" element={<Ireland />} />
           <Route path="/AboutMe" element={<AboutMe />} />
